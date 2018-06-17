a4bb630734f4efd7a2daa44d69f3c4519efe8f51
JungBin Kwon <wjdqls1342@naver.com>
2018-06-17 14:09:54 +0900

    Add files via upload

---
+++ comment/list.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const params = {
+  TableName: process.env.DYNAMODB_TABLE_COMMENT,
+};
+
+module.exports.list = (event, context, callback) => {
+  // fetch all comments from the database
+  dynamoDb.scan(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the comments.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin' : '*' },
+      body: JSON.stringify(result.Items),
+    };
+    callback(null, response);
+  });
+};
